76819b050b6c8c15b0cfa1890b02ae8caf133e3a
kamalbuilds <geniusamansingh@gmail.com>
2026-07-04 18:51:03 +0530

    Add standalone product header

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import { VestDrop } from "./pages/VestDrop";
 import "./styles.css";
 
-createRoot(document.getElementById("root")!).render(<VestDrop />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <header className="topbar">
+      <a className="brand" href="/"><span className="brand-mark">VD</span><span>VestDrop</span></a>
+    </header>
+    <VestDrop />
+  </>,
+);
